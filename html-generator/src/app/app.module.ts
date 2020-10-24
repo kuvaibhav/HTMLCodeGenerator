0f4b1a1136096989f9dcad75130b16ccca9c99d8
Kumar Vaibhav <kumar.vaibhav002@gmail.com>
2020-10-23 17:07:44 -0700

    Adding header and footer components

--- html-generator/src/app/app.module.ts
+++ html-generator/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
