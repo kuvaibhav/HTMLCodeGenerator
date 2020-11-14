219c939c6895a0b2b4437ddfe96619ba835cd6fc
Kumar Vaibhav <kumar.vaibhav002@gmail.com>
2020-11-14 14:53:35 -0800

    Setting up Apps page

--- html-generator/src/app/app.component.ts
+++ html-generator/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,83 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'html-generator';
+  sampleForm: FormGroup;
+  MOCK_DATA = [
+    {
+      id: '5efceffdsf',
+      objectName: 'obj1',
+      projectId: '3e848bheg728',
+      sourceType: 'DDL',
+      fields: [
+        {
+          keyName: 'dob',
+          dropdownValue: 'date'
+        },
+        {
+          keyName: 'name',
+          dropdownValue: 'varchar'
+        },
+        {
+          keyName: 'age',
+          dropdownValue: 'int'
+        }
+      ]
+    },
+    {
+      id: '5efceffdsf',
+      objectName: 'obj1',
+      projectId: '3e848bheg728',
+      sourceType: 'DDL',
+      fields: [
+        {
+          keyName: 'dob',
+          dropdownValue: 'date'
+        },
+        {
+          keyName: 'name',
+          dropdownValue: 'varchar'
+        },
+        {
+          keyName: 'age',
+          dropdownValue: 'int'
+        },
+        {
+          keyName: 'address',
+          dropdownValue: 'varchar'
+        },
+      ]
+    }
+  ]
+
+  ngOnInit() {
+    this.sampleForm = new FormGroup({});
+    this.initializeDDLForm();
+  }
+
+  initializeDDLForm() {
+    this.MOCK_DATA.forEach((domainObject, index) => {
+      let objectForm = new FormGroup({
+        objectId: new FormControl(domainObject.id),
+        projectId: new FormControl(domainObject.projectId),
+        objectName: new FormControl(domainObject.objectName),
+        sourceType: new FormControl(domainObject.sourceType),
+        fields: new FormArray([])
+      });
+      let fields = objectForm.get('fields') as FormArray;
+      domainObject.fields.forEach((field, index) => {
+        let fieldForm = new FormGroup({
+          keyName: new FormControl(field.keyName),
+          dropdownValue: new FormControl(field.dropdownValue)
+        });
+        fields.push(fieldForm);
+      })
+      this.sampleForm.addControl(String(index), objectForm);
+    });
+    console.log(this.sampleForm);
+  }
+
+  objectKeys(obj) {
+    return Object.keys(obj);
+  }
+
 }
